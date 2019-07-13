b382b446b2677b3e33b7f16c00751674a5a1a7e9
ZhenyaYahno <mr.yahnos@mail.ru>
2019-07-13 14:20:58 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,88 @@
+    window.onload = function (){
+
+
+        Vue.component('newnav', {
+    
+            props: {
+               postTitel: '',
+               
+
+            },
+            data(){
+                return {
+                 x: 20,
+                 
+                 display: 'none'
+                  
+                    
+                }
+            },
+            methods: {
+
+                clickHref: function($ev){
+                    let prom = new Promise((resolve,reject)=>{
+                        
+
+                        resolve(this.display)
+                    })
+                    console.log(this.display)
+                    if(this.display == 'none'){
+                        this.display = 'block';
+                    }
+
+                 },
+                 styleFnc: function(){
+                     console.log(this.display);
+                     return this.display;
+                     
+                 }
+                
+               
+            },
+            created: function(){
+            
+            },
+            
+            template: `
+            <a href="#" @click="clickHref"><nav>{{postTitel}}
+               <div class="BlueLine"  :style="{ display: styleFnc() }">
+                   <div class="BlueLineLeft"></div>
+                   <div class="BlueLineRight"></div>
+                  </div>
+            </nav>
+           
+            </a>
+            
+            
+            `
+        })
+            
+
+
+
+
+        var nav = new Vue ({
+            el: '#nav',
+            data: {
+                elems:[
+                  {id:1,postTitel:'Home',display: ''},
+                  {id:2,postTitel:'Contact',display: ''},
+                  {id:3,postTitel:'Pricing',display: ''},
+                  {id:4,postTitel:'Testimonials',display: ''},
+                  {id:5,postTitel:'About',display: ''},
+                  {id:6,postTitel:'Features',display: ''},
+                ] 
+                ,
+            },
+            methods:{
+              
+        
+            }
+          });
+
+        }
+
+
+        
+    
+
